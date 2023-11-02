b7d308ccc61c68c0623b7222f7f2cc7b6d38851a
hvega25 <2023_Herson.Vega@iticbcn.cat>
2023-11-02 18:53:53 +0100

    Empresa: Repositorio y metodo consulta todos los datos

--- Bolsa_Trabajo/src/main/java/principal/bolsa/controlador/Oferta_controlador.java
+++ Bolsa_Trabajo/src/main/java/principal/bolsa/controlador/Oferta_controlador.java
@@ -1,19 +1,42 @@
 package principal.bolsa.controlador;
 
 
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import principal.bolsa.dto.oferta;
+import principal.bolsa.repository.OfertaRepository;
+
+
 
 @RestController
 @RequestMapping("/oferta")
 public class Oferta_controlador {
 	
-	@GetMapping("/")
-	public String consultar() { 
-		return "hola";
-	}
+	@Autowired 
+	OfertaRepository ofertaRepositorio ; 
+	
+	@GetMapping("/consultar")
+    public List<oferta> getAllOfertas() {
+        return ofertaRepositorio.findAll();
+    }
+    
+    @PostMapping("/crear_oferta")
+    public ResponseEntity<?> crearOferta(@RequestBody oferta nuevaOferta) {
+    	return ofertaRepositorio.save(nuevaOferta);
+    }
+
 
 }
+
 
 
---
+++ Bolsa_Trabajo/src/main/java/principal/bolsa/repository/OfertaRepository.java
@@ -0,0 +1,17 @@
+package principal.bolsa.repository;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import principal.bolsa.dto.oferta;
+
+public interface OfertaRepository extends JpaRepository<oferta, Long>{
+	
+	public List<oferta> findAll();
+
+	
+
+
+}
