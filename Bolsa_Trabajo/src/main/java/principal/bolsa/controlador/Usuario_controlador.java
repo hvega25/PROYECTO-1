17e05bb4cae89c04240c52aee228d6ba1526cb59
4Tipy <leosteven2010@hotmail.com>
2023-11-03 15:28:14 +0100

    metodo eliminar por id completado

--- Bolsa_Trabajo/src/main/java/principal/bolsa/controlador/Usuario_controlador.java
+++ Bolsa_Trabajo/src/main/java/principal/bolsa/controlador/Usuario_controlador.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Usuario")
+@RequestMapping("/usuario")
 public class Usuario_controlador {
 
 	@GetMapping("/")
