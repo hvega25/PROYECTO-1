36d14e36b57ac2c6b0c99202040feff618e0760a
hvega25 <2023_herson.vega@iticbcn.cat>
2023-12-10 18:05:59 +0100

    Pruebas de persistencia y lectura del sql

---
+++ Bolsa_Trabajo/src/test/java/resources/import.sql
@@ -0,0 +1,8 @@
+-- Inserción de empresas
+INSERT INTO empresa (correo, direccion, nombre, telefono) VALUES ('empresa1@example.com', 'Dirección 1', 'Empresa Uno', '1234567890');
+INSERT INTO empresa (correo, direccion, nombre, telefono) VALUES ('empresa2@example.com', 'Dirección 2', 'Empresa Dos', '9876543210');
+
+-- Inserción de ofertas asociadas a empresas
+INSERT INTO oferta (nombre, descripcion, horario, puesto, jornada, fecha, empresa_id) VALUES ('Leo', 'Matar a un ruiseñor', '7am - 12 pm', 'frontend', 'mañana', '12 abril', 1); -- Asocia oferta a "Empresa Uno" (ID 1)
+INSERT INTO oferta (nombre, descripcion, horario, puesto, jornada, fecha, empresa_id) VALUES ('Luis', 'Matar a un ruiseñor ---- asdf', '7am - 12 pm', 'frontend', 'mañana', '12 abril', 1); -- Asocia oferta a "Empresa Uno" (ID 1)
+INSERT INTO oferta (nombre, descripcion, horario, puesto, jornada, fecha, empresa_id) VALUES ('Esto es un ejemplo', '2 de noviembre', '3am - 5 pm', 'matutina', 'pa la nasa', 'en 4', 2); -- Asocia oferta a "Empresa Dos" (ID 2)
