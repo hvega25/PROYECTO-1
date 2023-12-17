8dacc7cc3cd43fe5c8cbff4ebdfd953a3acb90d2
hvega25 <2023_herson.vega@iticbcn.cat>
2023-12-17 16:19:18 +0100

    USUARIO CONSULTAR

---
+++ Bolsa_Trabajo/src/main/java/principal/bolsa/repository/UsuarioRepositorio.java
@@ -0,0 +1,8 @@
+package principal.bolsa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import principal.bolsa.entity.Usuario;
+
+public interface UsuarioRepositorio extends JpaRepository <Usuario, Long> {
+
+}
