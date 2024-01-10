ea0cc61a47a8d3b5ce078a00a3de48ced1251f8a
hvega25 <2023_Herson.Vega@iticbcn.cat>
2024-01-10 16:07:46 +0100

    Version final 1.2: correción de errores y otras modificaciones

--- Bolsa_Trabajo/src/main/java/principal/bolsa/dto/UserRegisterDTO.java
+++ Bolsa_Trabajo/src/main/java/principal/bolsa/dto/UserRegisterDTO.java
@@ -1,4 +1,13 @@
 package principal.bolsa.dto;
 
+/*
+Ejemplo del json para registrarse
+{
+    "username": "espinoza",
+    "email": "espinoza@correo.com",
+    "password": "admin",
+    "password2": "admin"
+}
+ */
 public record UserRegisterDTO(String username, String email, String password, String password2) {
 }
